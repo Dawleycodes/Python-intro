c63b2a4b4d91aa82d7eceb3b38ad9bf349440409
Dawleycodes <felixdawley27@gmail.com>
2026-02-12 18:24:33 -0600

    Update age_checker.py

--- age_checker.py
+++ age_checker.py
@@ -1,14 +1,24 @@
 from datetime import datetime
 
 name = input("What is your name? ")
-birth_year = int(input("What year were you born? "))
+
+try:
+    birth_year = int(input("What year were you born? "))
+except ValueError:
+    print("Please enter a valid year (numbers only).")
+    exit()
 
 current_year = datetime.now().year
 age = current_year - birth_year
 
-print(f"Hello {name}")
+print(f"\nHello {name}")
 
-if age < 18:
+if age < 0:
+    print("You entered a future year 🤨")
+elif age < 18:
     print("You are a minor")
-else:
+elif age < 65:
     print("You are an adult")
+else:
+    print("You are a senior")
+
